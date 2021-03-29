388cfc99c9cd6bf0dbe30dd0686071540ce9b45c
nicolas.sassi <nicolas.sassi@b2wdigital.com>
2021-03-28 22:35:19 -0300

    first commit

---
+++ domain/entity/the_one_api/book/book.go
@@ -0,0 +1,13 @@
+package book
+
+import "context"
+
+type Book struct {
+	ID   string `json:"_id"`
+	Name string `json:"name"`
+}
+
+type Repository interface {
+	Get(ctx context.Context, id string) (Book, error)
+	List(ctx context.Context) ([]Book, error)
+}
